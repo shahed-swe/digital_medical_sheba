148d9227c965803344f66e27238d35a851aeca76
Md Shahed Talukder <shahedtalukder51@gmail.com>
2020-11-01 22:54:54 +0600

    new commit

--- main/admin.py
+++ main/admin.py
@@ -3,9 +3,13 @@ from .models import *
 # Register your models here.
 @admin.register(User)
 class User(admin.ModelAdmin):
-    list_display = ['username','is_patient','is_doctor','is_nurse','is_assistant','is_staff','is_superuser']
+    list_display = ['username','is_patient','is_doctor','is_nurse','is_assistant','is_admin','is_staff','is_superuser']
 
 admin.site.register(Patient)
 admin.site.register(Doctor)
 admin.site.register(Assistant)
 admin.site.register(Nurse)
+admin.site.register(Bill)
+admin.site.register(assignNurse)
+admin.site.register(assignMedicine)
+admin.site.register(assignAssistant)
--- main/models.py
+++ main/models.py
@@ -3,12 +3,14 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    is_patient = models.BooleanField(default=True)
-    is_doctor = models.BooleanField(default=True)
-    is_nurse = models.BooleanField(default=True)
-    is_assistant = models.BooleanField(default=True)
-    first_name = models.CharField(max_length=120)
-    last_name = models.CharField(max_length=120)
+    is_patient = models.BooleanField(default=False)
+    is_doctor = models.BooleanField(default=False)
+    is_nurse = models.BooleanField(default=False)
+    is_assistant = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_registered = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=120, blank=True, null=True)
+    last_name = models.CharField(max_length=120, blank=True, null=True)
 
 class Patient(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
@@ -18,6 +20,12 @@ class Patient(models.Model):
     email = models.CharField(max_length=50,blank=True,null=True)
     phone_no = models.CharField(max_length=50, blank=True, null=True)
 
+    class Meta:
+        db_table = "patient_info"
+    
+    def __str__(self):
+        return self.user.first_name + ' | ' + str(self.id)
+
 class Doctor(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
     user_name = models.CharField(max_length=50, blank=True, null=True)
@@ -25,6 +33,12 @@ class Doctor(models.Model):
     degree = models.CharField(max_length=120,blank=True,null=True)
     email = models.CharField(max_length=50, blank=True, null=True)
     phone_no = models.CharField(max_length=50, blank=True,null=True)
+
+    class Meta:
+        db_table = "doctor_info"
+    
+    def __str__(self):
+        return self.user.first_name + ' | '+str(self.id)
 
 class Nurse(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
@@ -34,6 +48,11 @@ class Nurse(models.Model):
     email = models.CharField(max_length=50, blank=True, null=True)
     phone_no = models.CharField(max_length=50,blank=True,null=True)
 
+    class Meta:
+        db_table = "nurse_info"
+
+    def __str__(self):
+        return self.user.first_name + ' | '+str(self.id)
 
 class Assistant(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
@@ -41,3 +60,66 @@ class Assistant(models.Model):
     address = models.CharField(max_length=500,blank=True,null=True)
     email = models.CharField(max_length=50, blank=True, null=True)
     phone_no = models.CharField(max_length=50,blank=True,null=True)
+
+    class Meta:
+        db_table = "assistant_info"
+
+    def __str__(self):
+        return self.user.first_name + ' | '+str(self.id)
+
+class Medicine(models.Model):
+    medicine_name = models.CharField(max_length=120,blank=True, null=True)
+    company_name = models.CharField(max_length=120,blank=True,null=True)
+
+    class Meta:
+        db_table = "medicine_info"
+
+    def __str__(self):
+        return self.medicine_name + ' | '+ str(self.id)
+
+class Feedback(models.Model):
+    feedback = models.CharField(max_length=120, blank=True, null=True)
+
+    class Meta:
+        db_table = "feedback_section"
+    
+    def __str__(self):
+        return self.feedback + ' | '+ str(self.id)
+
+class Bill(models.Model):
+    patient = models.OneToOneField(Patient, on_delete=models.CASCADE, primary_key=True)
+    bill = models.CharField(max_length=120,blank=True,null=True)
+
+    class Meta:
+        db_table = "billing_information"
+    
+
+class assignNurse(models.Model):
+    patient = models.OneToOneField(Patient, on_delete=models.CASCADE, primary_key=True)
+    nurse = models.ForeignKey(Nurse, on_delete=models.SET_NULL,blank=True,null=True,related_name='assign_nurse')
+
+    class Meta:
+        db_table = "assigned_nurse"
+    
+    def __str__(self):
+        return self.patient.user.first_name + ' | '+ self.nurse.user.first_name + ' | '+ str(self.id)
+
+class assignMedicine(models.Model):
+    patient = models.OneToOneField(Patient, on_delete=models.CASCADE, primary_key=True)
+    medicine = models.ForeignKey(Medicine, on_delete=models.SET_NULL, blank=True, null=True, related_name="assign_medicine")
+
+    class Meta:
+        db_table = "assigned_medicine"
+    
+    def __str__(self):
+        return self.patient.user.first_name + ' | '+ self.medicine.medicine_name + ' | '+ str(self.id)
+
+class assignAssistant(models.Model):
+    doctor = models.OneToOneField(Doctor, on_delete=models.CASCADE, primary_key=True)
+    assistant = models.ForeignKey(Assistant, on_delete=models.CASCADE, related_name="assign_assistant")
+
+    class Meta:
+        db_table = "assigned_assistant"
+
+    def __str__(self):
+        return self.doctor.full_name + ' | '+self.assistant.user.first_name + ' | '+str(self.id)
