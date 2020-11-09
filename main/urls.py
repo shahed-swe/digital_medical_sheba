feabdb363f7561f9b6d6b0e104362e334777c90c
Md Shahed Talukder <35971677+shahed-swe@users.noreply.github.com>
2020-11-09 12:59:11 +0600

    new commit

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 from rest_framework import routers
+from django.conf.urls import include
+router = routers.DefaultRouter()
+router.register(r'úser',views.UserViewSet)
+
+
 
 urlpatterns = [
     path('',views.home, name="home"),
+    path('user/',include(router.urls)),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from . import serializers
+from . import models
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from rest_framework import viewsets,status
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializers_class = serializers.User
 
 # Create your views here.
 def home(request):
