08e4f1ae3fe6b95784ce32a53b6388c0aaa54912
Md Shahed Talukder <35971677+shahed-swe@users.noreply.github.com>
2020-12-05 21:57:14 +0600

    patient views created

--- patient/urls.py
+++ patient/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.conf.urls import include
+from . import views
 
 urlpatterns = [
-    
+    path('', views.patient, name="patient"),
+    path('mylogin/', views.login, name="login"),
+    path('myregistration/', views.registration, name="registration"),
 ]
